6a134a826d58652dc5288e09909774e5c2f60aa2
prajwal <prajwalshah912@gmail.com>
2023-07-24 01:06:40 +0530

    post data transform through map operator in console

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,30 +8,43 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
- loadedPost = []
+  loadedPost = []
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  onCreatePost(postData:{title:string, content:string}){
+  onCreatePost(postData: { title: string, content: string }) {
     this.http.post('https://http-client-backend-default-rtdb.firebaseio.com/posts.json',
-     postData
-     ).subscribe(res => {
+      postData
+    ).subscribe(res => {
       console.log(res)
-     })
-   }
+    })
+  }
 
-   onFetchPosts(){
+  onFetchPosts() {
     this.fetchPosts()
-   }
+  }
 
   ngOnInit() {
     this.fetchPosts()
   }
 
 
-  private fetchPosts(){
-    this.http.get("https://http-client-backend-default-rtdb.firebaseio.com/posts.json").subscribe(res => {
-      console.log(res)
-    })
+  private fetchPosts() {
+    this.http
+      .get<any>("https://http-client-backend-default-rtdb.firebaseio.com/posts.json")
+      .pipe(
+        map(res => {
+          let postData = [];
+          for (let item in res){
+            if(res.hasOwnProperty(item)){
+              postData.push({...res[item], id:item})
+            }
+          }
+             return postData
+        }))
+      .subscribe(res => {
+        console.log(res);
+      });
   }
+
 }
