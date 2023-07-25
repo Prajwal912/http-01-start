bf1afb1e0d758e7ea4651015178c9b956e8d665a
prajwal <prajwalshah912@gmail.com>
2023-07-26 00:38:48 +0530

    queryparams ans multi params

--- http-01-start/src/app/http.service.ts
+++ http-01-start/src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.mode';
 import { Subject } from 'rxjs';
@@ -13,44 +13,52 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
 
-  createAndStorePost(title:string, content:string){
-    let postData = {tit:title, cont:content};
-    this.http.post<{name:string}>('https://http-client-backend-default-rtdb.firebaseio.com/posts.json',
-    postData
-  ).subscribe(res => {
-    console.log(res)
-  },
-  error => {
-    this.errorSubject.next(error.message)
-  })
+  createAndStorePost(title: string, content: string) {
+    let postData = { tit: title, cont: content };
+    this.http.post<{ name: string }>('https://http-client-backend-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe(res => {
+      console.log(res)
+    },
+      error => {
+        this.errorSubject.next(error.message)
+      })
   }
 
-  fetchPosts(){
-   return  this.http
-    .get<{[item:string]: Post}>("https://http-client-backend-default-rtdb.firebaseio.com/posts.json",
-    {
-      headers: new HttpHeaders(
+  fetchPosts() {
+    // set multiple query params -- not worked with firebase
+    //using append we can add multiple params
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print1', 'hello')
+    searchParams = searchParams.append('print2', 'hey')
+
+    return this.http
+      .get<{ [item: string]: Post }>("https://http-client-backend-default-rtdb.firebaseio.com/posts.json",
         {
-          'Auth': 'nothing to say',
-          'api-res': 'https://http-client-backend-default-rtdb.firebaseio.com/',
+          headers: new HttpHeaders(
+            {
+              'Auth': 'nothing to say',
+              'api-res': 'https://http-client-backend-default-rtdb.firebaseio.com/',
+            }),
+          params: searchParams
+          // params: new HttpParams().set('print', 'pretty')
         })
-    })
-    .pipe(
-      map((res) => {
-        let postData:Post[] = [];
-        for (let item in res){
-          if(res.hasOwnProperty(item)){
-            postData.push({...res[item], id:item})
+      .pipe(
+        map((res) => {
+          let postData: Post[] = [];
+          for (let item in res) {
+            if (res.hasOwnProperty(item)) {
+              postData.push({ ...res[item], id: item })
+            }
           }
-        }
-           return postData
-      } , (error: { statusText: string; }) => {
-        this.errorSubject.next(error.statusText)
-      }))
-    }
+          return postData
+        }, (error: { statusText: string; }) => {
+          this.errorSubject.next(error.statusText)
+        }))
+  }
 
 
-    deletePost(){
-    return  this.http.delete("https://http-client-backend-default-rtdb.firebaseio.com/posts.json")
-    }
+  deletePost() {
+    return this.http.delete("https://http-client-backend-default-rtdb.firebaseio.com/posts.json")
+  }
 }
