4962415074b5ae3bc84f155d6cba6010ec0a5b69
prajwal <prajwalshah912@gmail.com>
2023-07-26 22:37:08 +0530

    requests interceptors

--- http-01-start/src/app/auth-interceptors.service.ts
+++ http-01-start/src/app/auth-interceptors.service.ts
@@ -6,11 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthInterceptorsService implements HttpInterceptor {
-
+//requests interceptors
 //req is used for taking the req you wasnt to use then next is used to forwarding the req to the server or getting back res
   intercept(req: HttpRequest<any>, next: HttpHandler){
     console.log('request is on its way before req is gone through the appication it goes from the intercept globally');
-    return next.handle(req)
+    console.log(req.url)// it accesses the url in http service directly
+    const modifiedReq = req.clone({
+      headers: req.headers.append('Auth', 'xyz')// it accesses the url and the headers in http service directly
+    })
+    return next.handle(modifiedReq)
   }
   constructor() { }
 }
